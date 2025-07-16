e9b42d72bc7320ef6042cf684e7305ac87ae57d0
R4CHIT <tiwarirachit50@gmail.com>
2025-07-16 17:53:51 +0545

    update

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -5,12 +5,12 @@ import Cartcard from "./Cartcard";
 const Cart = () => {
   
   const data =localStorage.getItem("cart")
-  const [item, setItem] = useState(JSON.parse(data));
-  const itemcount = JSON.parse(localStorage.getItem('cart')).length
+  const [item, setItem] = useState(data ? JSON.parse(data) : []);
+  
   return (
     <div className="md:px-10 lg:px-20 bg-whiten mt-30  ">
         <Navbar />
-        {itemcount == 0 || item == null ? (
+        {item.length === 0 ? (
           <div className="flex justify-center items-center h-[60vh] transform duration-600 text-3xl md:text-5xl text-pink-500">No item in Cart</div>
         ) : (
           <div className="grid grid-cols-1 gap-3">
