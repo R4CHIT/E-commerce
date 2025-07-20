7df81c48a213369b51d1119c7bc019c1dae6e50e
R4CHIT <tiwarirachit50@gmail.com>
2025-07-20 10:39:52 +0545

    responsive

--- src/components/Product/Popularitem.jsx
+++ src/components/Product/Popularitem.jsx
@@ -13,7 +13,7 @@ function Popularitem() {
   return (
     <>
       
-        <div className="mx-20 md:px-10 lg:px-20 bg-whiten mt-10">
+        <div className="mx-10 md:px-10 lg:px-20 bg-whiten mt-10">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-10">
             {productData.map((item) => (
             <Card item={item} key={item.id} />
